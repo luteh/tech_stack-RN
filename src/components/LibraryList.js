eb2cba582481f25562cfb66e95cc39c59bad2776
luteh <luthfanmaftuh@gmail.com>
2017-06-12 14:09:20 +0700

    Added component LibraryList

---
+++ src/components/LibraryList.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Luteh on 12/06/2017.
+ */
+import React, {Component} from 'react';
+
+class LibraryList extends Component {
+    render() {
+        return;
+    };
+}
+
+export default LibraryList;
