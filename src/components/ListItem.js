a3b0fdce083e6140805c46f6aba20cd54a38c73f
luteh <luthfanmaftuh@gmail.com>
2017-06-13 14:02:02 +0700

    return item from ListItem

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -2,10 +2,16 @@
  * Created by Luteh on 12/06/2017.
  */
 import React, {Component} from 'react';
+import {Text} from 'react-native';
+import {CardSection} from './common';
 
-class ListItem extends Component{
-    render(){
-
+class ListItem extends Component {
+    render() {
+        return (
+            <CardSection>
+                <Text>{this.props.library.title}</Text>
+            </CardSection>
+        );
     }
 }
 
